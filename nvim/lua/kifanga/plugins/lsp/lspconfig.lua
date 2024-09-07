b69763bd88ac77f43af5c3177208916277b5daae
Kifanga Mukundi <pkmymcmbnhs@gmail.com>
2024-09-07 15:45:13 +0300

    fixed the if statement for tsserver

--- nvim/lua/kifanga/plugins/lsp/lspconfig.lua
+++ nvim/lua/kifanga/plugins/lsp/lspconfig.lua
@@ -87,13 +87,14 @@ return {
     mason_lspconfig.setup_handlers({
       -- default handler for installed servers
       function(server_name)
+        if server_name == "tsserver" then
+          server_name = "ts_ls"
+        end
+      
         lspconfig[server_name].setup({
-          if server_name == "tsserver" then
-						server_name = "ts_ls"
-					end
           capabilities = capabilities,
         })
-      end,
+      end,      
       ["emmet_ls"] = function()
         -- configure emmet language server
         lspconfig["emmet_ls"].setup({
