12f2267950edded16b9873c058f0a2de48e5a03d
Kifanga Mukundi <pkmymcmbnhs@gmail.com>
2024-09-14 13:29:48 +0300

    optimized weztern, added harpoon and fixed ts ls'

--- nvim/lua/kifanga/plugins/lsp/lspconfig.lua
+++ nvim/lua/kifanga/plugins/lsp/lspconfig.lua
@@ -87,10 +87,6 @@ return {
     mason_lspconfig.setup_handlers({
       -- default handler for installed servers
       function(server_name)
-        if server_name == "tsserver" then
-          server_name = "ts_ls"
-        end
-      
         lspconfig[server_name].setup({
           capabilities = capabilities,
         })
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -4,42 +4,21 @@ local wezterm = require("wezterm")
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices
-
--- My coolnight colorscheme
-config.colors = {
-	foreground = "#CBE0F0",
-	background = "#011423",
-	cursor_bg = "#47FF9C",
-	cursor_border = "#47FF9C",
-	cursor_fg = "#011423",
-	selection_bg = "#033259",
-	selection_fg = "#CBE0F0",
-	ansi = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#0FC5ED", "#a277ff", "#24EAF7", "#24EAF7" },
-	brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
-}
-
--- Set the wallpaper path using the home directory
-config.window_background_image = os.getenv("HOME") .. "/.config/wezterm/wallpaper.jpg"
-
-config.window_background_image_hsb = {
-	brightness = 0.15,  -- Darken the background image by reducing it to 1/3rd
-	hue = 1.0,         -- Adjust the hue
-	saturation = 1.0,  -- Adjust the saturation
-}
+-- Apply the Catppuccin Mocha color scheme
+config.color_scheme = "Catppuccin Mocha"
 
 -- Configure fonts
 config.font = wezterm.font_with_fallback({
-	{ family = 'JetBrains Mono', weight = 'Regular' },
-	{ family = 'JetBrains Mono', weight = 'Bold', italic = true },
-	{ family = 'JetBrains Mono', weight = 'ExtraBold' },
+  { family = 'JetBrains Mono', weight = 'Regular' },
+  { family = 'JetBrains Mono', weight = 'Bold', italic = true },
+  { family = 'JetBrains Mono', weight = 'ExtraBold' },
 })
 
-config.font_size = 14.5
+config.font_size = 12
 config.enable_tab_bar = false
-config.window_decorations = "RESIZE"
-config.window_background_opacity = 0.80
-config.macos_window_background_blur = 10
 
--- And finally, return the configuration to wezterm
+-- Rendering Method: OpenGL, Software, WebGpu
+config.front_end = "Software"
+
+-- Return the configuration to wezterm
 return config
