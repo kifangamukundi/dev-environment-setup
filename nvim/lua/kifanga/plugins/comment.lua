b41f6a45575c16d5609ae1e99218ef7df31d4a32
Kifanga Mukundi <pkmymcmbnhs@gmail.com>
2024-09-15 14:39:13 +0300

    keymaps changed for telescope and comment better keymaps

--- nvim/lua/kifanga/plugins/comment.lua
+++ nvim/lua/kifanga/plugins/comment.lua
@@ -5,15 +5,45 @@ return {
     "JoosepAlviste/nvim-ts-context-commentstring",
   },
   config = function()
-    -- import comment plugin safely
+    -- Import Comment plugin safely
     local comment = require("Comment")
-
     local ts_context_commentstring = require("ts_context_commentstring.integrations.comment_nvim")
 
-    -- enable comment
+    -- Enable comment with custom configuration
     comment.setup({
-      -- for commenting tsx, jsx, html files
+      ---Add a space between comment and the line
+      padding = true,
+
+      ---Lines to be ignored while comment/uncomment.
+      ignore = nil,
+
+      ---Create basic (operator-pending) and extended mappings for NORMAL + VISUAL mode
+      mappings = {
+        basic = true,  -- operator-pending mapping like gcc, gcb
+        extra = true,  -- extra mappings like gco, gcO
+        extended = false, -- extended mappings like g>, g<
+      },
+
+      ---LHS of toggle mapping in NORMAL + VISUAL mode
+      toggler = {
+        line = '<leader>gcc',  -- line-comment keymap
+        block = '<leader>gbc', -- block-comment keymap
+      },
+
+      ---LHS of operator-pending mapping in NORMAL + VISUAL mode
+      opleader = {
+        line = '<leader>gc',   -- leader for line-comment
+        block = '<leader>gb',  -- leader for block-comment
+      },
+
+      ---Pre-hook for tsx, jsx, html files
       pre_hook = ts_context_commentstring.create_pre_hook(),
+
+      ---Post-hook, called after commenting is done
+      post_hook = nil,
     })
+
+    -- Set up ts-context-commentstring plugin
+    require('ts_context_commentstring').setup()
   end,
 }
--- nvim/lua/kifanga/plugins/telescope.lua
+++ nvim/lua/kifanga/plugins/telescope.lua
@@ -42,14 +42,14 @@ return {
     local keymap = vim.keymap
 
     keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<CR>", { desc = "Find files" })
-    keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<CR>", { desc = "Live grep" })
+    keymap.set("n", "<leader>fl", "<cmd>Telescope live_grep<CR>", { desc = "Live grep" })
     keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<CR>", { desc = "Find buffers" })
     keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<CR>", { desc = "Find help tags" })
     keymap.set("n", "<leader>fc", "<cmd>Telescope current_buffer_fuzzy_find<CR>", { desc = "Fuzzy find in current buffer" })
-    keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<CR>", { desc = "Find recent files" })
-    keymap.set("n", "<leader>fhc", "<cmd>Telescope command_history<CR>", { desc = "Command history" })
+    keymap.set("n", "<leader>fo", "<cmd>Telescope oldfiles<CR>", { desc = "Find recent files" })
+    keymap.set("n", "<leader>fy", "<cmd>Telescope command_history<CR>", { desc = "Command history" })
     keymap.set("n", "<leader>fm", "<cmd>Telescope marks<CR>", { desc = "Find marks" })
-    keymap.set("n", "<leader>frg", "<cmd>Telescope registers<CR>", { desc = "Find registers" })
+    keymap.set("n", "<leader>fg", "<cmd>Telescope registers<CR>", { desc = "Find registers" })
     keymap.set("n", "<leader>fd", "<cmd>Telescope commands<CR>", { desc = "Find commands" })
     keymap.set("n", "<leader>fk", "<cmd>Telescope keymaps<CR>", { desc = "Find keymaps" })
     keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
