aa49393cdec9722818b51921441d1c7901b791a9
kifangamukundi <pkmymcmbnhs@gmail.com>
2025-03-19 20:21:57 -0400

    improved neotree config

--- nvim/lua/kifanga/core/keymaps.lua
+++ nvim/lua/kifanga/core/keymaps.lua
@@ -1,6 +1,6 @@
 vim.g.mapleader = " "
 
-local keymap = vim.keymap -- for conciseness
+local keymap = vim.keymap
 
 -- Disable arrow keys in all modes
 local modes = { "n", "i", "v", "s", "c", "t" }
@@ -17,7 +17,7 @@ end
 function ClearRegisters()
   local registers = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '-'}
   for _, reg in ipairs(registers) do
-    vim.fn.setreg(reg, '')  -- Clear the register
+    vim.fn.setreg(reg, '')
   end
 end
 
@@ -110,10 +110,10 @@ keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" })
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" })
 
-keymap.set("n", "<leader>mh", "<C-w>h", { desc = "Move to the left split" })  -- left
-keymap.set("n", "<leader>mj", "<C-w>j", { desc = "Move to the below split" })  -- down
-keymap.set("n", "<leader>mk", "<C-w>k", { desc = "Move to the above split" })  -- up
-keymap.set("n", "<leader>ml", "<C-w>l", { desc = "Move to the right split" })  -- right
+keymap.set("n", "<leader>mh", "<C-w>h", { desc = "Move to the left split" })
+keymap.set("n", "<leader>mj", "<C-w>j", { desc = "Move to the below split" })
+keymap.set("n", "<leader>mk", "<C-w>k", { desc = "Move to the above split" })
+keymap.set("n", "<leader>ml", "<C-w>l", { desc = "Move to the right split" })
 
 -- Keymaps for buffer management
 keymap.set("n", "<leader>bo", "<cmd>enew<CR>", { desc = "Open new buffer" })
